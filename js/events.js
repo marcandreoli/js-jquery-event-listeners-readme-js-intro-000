9f193e6185aa1d09db1040b15fff57510b3464e3
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-04-17 10:09:35 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $(document).on("keydown", function(key) {
+    if(key.which ==71) {
+      alert("You pressed G");
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on(
+    "submit", function() {
+
+        alert("Your form is going to be submitted now.")
+
+    }
+  )
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  return getIt();
+  return frameIt();
+  return pressIt();
+  return submitIt();
 });
